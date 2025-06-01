71ba5d70736bc461cadb714ad42642b273f2c4c7
Eramaloby <karachun04@gmail.com>
2025-06-01 14:14:59 +0300

    added extra reducers

--- src/apiService.ts
+++ src/apiService.ts
@@ -1,23 +1,27 @@
 import axios from 'axios';
 import { WeatherApiResponce, WeatherApiData } from './types/types';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const API_KEY = process.env.REACT_APP_OPENWEATHER_API_KEY;
 const CITY_ID = 524901;
 
-export const fetchWeather = async (): Promise<WeatherApiData> => {
-  if (!API_KEY) {
-    throw new Error(
-      'API_ERROR is not defined. Set REACT_APP_OPENWEATHER_API_KEY in .env file.'
-    );
-  }
+export const fetchWeather = createAsyncThunk<WeatherApiData>(
+  'weather/fetchWeather',
+  async () => {
+    if (!API_KEY) {
+      throw new Error(
+        'API_ERROR is not defined. Set REACT_APP_OPENWEATHER_API_KEY in .env file.'
+      );
+    }
 
-  try {
-    const responce: WeatherApiResponce = await axios.get(
-      `http://api.openweathermap.org/data/2.5/forecast?id=${CITY_ID}&appid=${API_KEY}&units=metric`
-    );
-    console.log('data', responce.data);
-    return responce.data;
-  } catch (error) {
-    throw error;
+    try {
+      const responce: WeatherApiResponce = await axios.get(
+        `http://api.openweathermap.org/data/2.5/forecast?id=${CITY_ID}&appid=${API_KEY}&units=metric`
+      );
+      console.log('data', responce.data);
+      return responce.data;
+    } catch (error) {
+      throw error;
+    }
   }
-};
+);
--- src/features/weather/weatherSlice.tsx
+++ src/features/weather/weatherSlice.tsx
@@ -3,6 +3,8 @@ import type { PayloadAction } from '@reduxjs/toolkit';
 
 import { WeatherState } from '../../types/types';
 
+import { fetchWeather } from '../../apiService';
+
 const initialState: WeatherState = {
   data: null,
   city: '',
@@ -24,7 +26,27 @@ export const weatherSlice = createSlice({
         if (state.history.length > 10) {
           state.history.pop();
         }
+      } else {
+        const cityIndex = state.history.indexOf(newCity);
+        if (cityIndex > -1) {
+          state.history = state.history.splice(cityIndex);
+          state.history.unshift(newCity);
+        }
       }
     },
+    setCity: (state: WeatherState, action: PayloadAction<string>) => {
+      const newCity = action.payload;
+      if (newCity !== state.city) {
+        state.city = newCity;
+      }
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchWeather.pending, (state: WeatherState) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(fetchWeather.fulfilled, (state: WeatherState, action) => {});
   },
 });
